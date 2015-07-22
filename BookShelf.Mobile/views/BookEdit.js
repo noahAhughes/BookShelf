21851600c50e608f6242eacc16e0b5e84229da19
Artem Tabalin <tabalinas@gmail.com>
2015-07-22 17:14:24 +0300

    Views: Fix book status change issue

--- BookShelf.Mobile/views/BookEdit.js
+++ BookShelf.Mobile/views/BookEdit.js
@@ -3,8 +3,13 @@
     return $.extend(BookShelf.BookForm(params), {
                 
         save: function() {
+            var bookStatus = BookShelf.db.getBookStatus(BookShelf.db.books.get(this.getBook().id));
             BookShelf.db.books.update(this.getBook());
-            BookShelf.app.bookListShowing.fire({ reloadBook: this.getBook().id });
+            if(BookShelf.db.getBookStatus(this.getBook()) === bookStatus) {
+                BookShelf.app.bookListShowing.fire({ reloadBook: this.getBook().id });
+            } else {
+                BookShelf.app.bookListShowing.fire({ reload: true });
+            }
             BookShelf.app.back();
         }
 
